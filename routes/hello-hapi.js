97f3fe701a3476fa21dbcbf33584028f2352ee8d
xianengqi <920986617@qq.com>
2018-12-20 17:01:16 +0800

    为 REST 接口添加 Swagger 标记

--- routes/hello-hapi.js
+++ routes/hello-hapi.js
@@ -5,5 +5,9 @@ module.exports = [
     handler: (request, reply) => {
       reply('hapi');
     },
+    config: {
+      tags: ['api', 'tests'],
+      description: '测试hello-hapi',
+    },
   },
 ];
