87d7a5a638f131a3342b0ea87efdda29d781d19b
xianengqi <920986617@qq.com>
2018-12-20 17:00:13 +0800

    在 app.js 中，我们通过 server.register 挂载 swagger 插件配置

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ require('env2')('./.env');
 const config = require('./config');
 const routesHelloHapi = require('./routes/hello-hapi');
 
-const server = new Hapi.Server()
+// 引入自定义的 hapi-swagger 插件配置
+const pluginHapiSwagger = require('./plugins/hapi-swagger');
 
+const server = new Hapi.Server();
 // 配置服务器启动 host 与端口
 server.connection({
   port: config.port,
@@ -12,6 +14,10 @@ server.connection({
 });
 
 const init = async () => {
+  await server.register([
+    // 为系统使用 hapi-swagger
+    ...pluginHapiSwagger,
+  ]);
   server.route([
     // 创建一个简单的 hello hapi 接口
     ...routesHelloHapi,
