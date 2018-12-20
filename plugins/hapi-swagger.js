03641017c79fa3303b67b9e27292f1bfd7c151ab
xianengqi <920986617@qq.com>
2018-12-20 16:59:37 +0800

    添加swagger插件配置

---
+++ plugins/hapi-swagger.js
@@ -0,0 +1,23 @@
+const inert = require('inert');
+const vision = require('vision');
+const package = require('package');
+const hapiSwagger = require('hapi-swagger');
+
+module.exports = [
+  inert,
+  vision,
+  {
+    register: hapiSwagger,
+    options: {
+      info: {
+        title: '接口文档',
+        version: package.version,
+      },
+      // 定义接口以 tags 属性定义为分组
+      grouping: 'tags',
+      tags: [
+        { name: 'tests', description: '测试相关' },
+      ],
+    },
+  },
+];
