06fe257627392d448d62077016b0967d91bd2e02
xianengqi <920986617@qq.com>
2018-12-23 14:43:24 +0800

    定义数据库业务相关的 model

---
+++ models/nideshop_feedback.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => sequelize.define(
+  'nideshop_feedback',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: false,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    connect: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    msg_content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    msg_time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    tableName: 'nideshop_feedback'
+  }
+)
